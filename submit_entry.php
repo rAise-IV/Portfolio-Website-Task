c4a4e1322e989dc87a29d79557e5a9ba27475f46
rAise-IV <kristian.sarong@gmail.com>
2023-04-25 06:43:09 +0100

    allows for entry submission to MySQL

---
+++ submit_entry.php
@@ -0,0 +1,31 @@
+<?php
+
+    $host = "127.0.0.1";
+    $dbusername = "root";
+    $dbpassword = "";
+    $dbname = "ec22959";
+
+    $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+
+    // Check if there was an error connecting to the database
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Escape user inputs for security
+    /* It replaces characters such as quotes and backslashes with their escape sequences,
+    so that they can be safely used in a query without altering its syntax or functionality. */ 
+    $title = mysqli_real_escape_string($conn, $_POST['title']);
+    $text = mysqli_real_escape_string($conn, $_POST['entry-text']);
+
+    // Attempt insert query execution
+    $sql = "INSERT INTO blogEntry (title, text) VALUES ('$title', '$text')";
+    if(mysqli_query($conn, $sql)){
+        header('Location: blog.php');
+        exit;
+    } else{
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
+?>
